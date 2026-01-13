6e8833cdef7a041368cd222582b5aad033c129a6
enochevkina <elizaveta.nochevkina@gmail.com>
2026-01-13 22:57:10 +0300

    Update ex.19

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,18 +1,22 @@
 from requests import Response
 import json
+import allure
 from datetime import datetime
 from lib.my_requests import MyRequests
 from lib.assertions import Assertions
 
 class BaseCase:
+    @allure.step("Get cookie '{cookie_name}' from response")
     def get_cookie (self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"Cannot find cookie with  name {cookie_name} in the last response"
         return response.cookies[cookie_name]
 
+    @allure.step("Get header '{headers_name}' from response")
     def get_header (self, response: Response, headers_name):
         assert headers_name in response.headers, f"Cannot find header with  name {headers_name} in the last response"
         return response.headers[headers_name]
 
+    @allure.step("Get JSON value '{name}' from response")
     def get_json_value(self, response: Response, name):
         try:
             response_as_dict = response.json()
@@ -23,6 +27,7 @@ class BaseCase:
 
         return response_as_dict[name]
 
+    @allure.step("Prepare registration data")
     def prepare_registration_data(self, email = None):
         if email is None:
             base_part = "learnqa"
@@ -36,6 +41,8 @@ class BaseCase:
             'lastName': 'learnqa',
             'email': email
         }
+
+    @allure.step("Create new user")
     def create_user(self):
         register_data = self.prepare_registration_data()
         response = MyRequests.post("/user", data=register_data)
@@ -49,6 +56,7 @@ class BaseCase:
             "password": register_data["password"]
         }
 
+    @allure.step("Login user '{email}'")
     def login_user(self, email, password):
 
         response = MyRequests.post(
@@ -65,6 +73,7 @@ class BaseCase:
             "token": response.headers["x-csrf-token"]
         }
 
+    @allure.step("Create and login new user")
     def create_and_login_user(self):
         user = self.create_user()
         auth = self.login_user(user["email"], user["password"])
