372c78f39762cc2c8506969f90e4775def715216
enochevkina <elizaveta.nochevkina@gmail.com>
2026-01-13 22:52:01 +0300

    Update ex.19

--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -1,25 +1,27 @@
-import pytest
+import allure
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
+
+@allure.epic("User deletion cases")
 class TestUserDelete(BaseCase):
 
+    @allure.description("This test checks that undeletable system user cannot be deleted")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_user_delete_undeletable_user(self):
-        # данные пользователя
         email = "vinkotov@example.com"
-        password =  "1234"
+        password = "1234"
         user_id = "2"
 
-        #логин под этим пользователем
         user_data = self.login_user(email, password)
 
-        # попытка удаления пользователя, id нам известен из задания
-        response = MyRequests.delete(
-            f"/user/{user_id}",
-            headers={"x-csrf-token": user_data["token"]},
-            cookies={"auth_sid": user_data["auth_sid"]}
-        )
+        with allure.step("Try to delete undeletable system user"):
+            response = MyRequests.delete(
+                f"/user/{user_id}",
+                headers={"x-csrf-token": user_data["token"]},
+                cookies={"auth_sid": user_data["auth_sid"]}
+            )
 
         Assertions.assert_code_status(response, 400)
         Assertions.assert_json_value_by_name(
@@ -29,42 +31,45 @@ class TestUserDelete(BaseCase):
             f"Unexpected response content {response.content}"
         )
 
+    @allure.description("This test checks that just created user can be successfully deleted")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_user_delete_just_created_user(self):
-        # создание и логин с новым пользователем
+
         user_data = self.create_and_login_user()
 
-        # удаление пользователя
-        response = MyRequests.delete(
-            f"/user/{user_data['user_id']}",
-            headers={"x-csrf-token": user_data["token"]},
-            cookies={"auth_sid": user_data["auth_sid"]}
-        )
+        with allure.step("Delete created user"):
+            response = MyRequests.delete(
+                f"/user/{user_data['user_id']}",
+                headers={"x-csrf-token": user_data["token"]},
+                cookies={"auth_sid": user_data["auth_sid"]}
+            )
 
         Assertions.assert_code_status(response, 200)
 
-        # попытка получения данных пользователя
-        response = MyRequests.get(
-            f"/user/{user_data['user_id']}",
-            headers={"x-csrf-token": user_data["token"]},
-            cookies={"auth_sid": user_data["auth_sid"]}
-        )
+        with allure.step("Try to get deleted user data"):
+            response = MyRequests.get(
+                f"/user/{user_data['user_id']}",
+                headers={"x-csrf-token": user_data["token"]},
+                cookies={"auth_sid": user_data["auth_sid"]}
+            )
 
         Assertions.assert_code_status(response, 404)
         assert response.content.decode("utf-8") == "User not found", \
             f"Unexpected response content {response.content}"
 
+    @allure.description("This test checks that user cannot delete another user's account")
+    @allure.severity(allure.severity_level.NORMAL)
     def test_user_delete_user_as_different_user(self):
 
-        # создание пользователей и логин
         user_data_1 = self.create_and_login_user()
         user_data_2 = self.create_and_login_user()
 
-        # попытка удаления 1-го пользователя с данными 2-го
-        response = MyRequests.delete(
-            f"/user/{user_data_1['user_id']}",
-            headers={"x-csrf-token": user_data_2["token"]},
-            cookies={"auth_sid": user_data_2["auth_sid"]}
-        )
+        with allure.step("Try to delete first user using second user's credentials"):
+            response = MyRequests.delete(
+                f"/user/{user_data_1['user_id']}",
+                headers={"x-csrf-token": user_data_2["token"]},
+                cookies={"auth_sid": user_data_2["auth_sid"]}
+            )
 
         Assertions.assert_code_status(response, 400)
         Assertions.assert_json_value_by_name(
@@ -73,5 +78,3 @@ class TestUserDelete(BaseCase):
             "This user can only delete their own account.",
             f"Unexpected response content {response.content}"
         )
-
-
